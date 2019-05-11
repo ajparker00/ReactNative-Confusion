ee2a92f595fea5d973986f1c2bf8befba6aae7be
Andre <ajparker000@gmail.com>
2019-05-10 22:14:54 -0700

    Activity Indicator

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -12,10 +13,23 @@ const mapStateToProps = state => {
         leaders: state.leaders
     }
 }
-
+//adding activity indicator had an extra bracket in the else if statement
 function RenderItem(props) {
     const item = props.item;
-    if (item != null) {
+
+    if (props.isLoading) {
+        return(
+                <Loading />
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <View> 
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
+    else if (item != null) {
         return (
             <Card
                 featuredTitle={item.name}
@@ -41,9 +55,18 @@ class Home extends Component {
     render() {
         return (
             <ScrollView>
-                <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} />
-                <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} />
-                <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} />
+                 <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess} 
+                    />
+                <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess} 
+                    />
+                <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                    isLoading={this.props.leaders.isLoading}
+                    errMess={this.props.leaders.errMess} 
+                    />
             </ScrollView>
         );
     }
